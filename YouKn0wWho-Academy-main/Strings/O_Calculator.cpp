da83cbe915b146e32d599cb66841f99b78095c6e
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-20 00:30:01 +0600

    Initial commit

--- YouKn0wWho-Academy-main/Strings/O_Calculator.cpp
+++ YouKn0wWho-Academy-main/Strings/O_Calculator.cpp
@@ -9,17 +9,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
 #define int long long
 #define endl '\n'
 
-const int MOD = 1e9+7, INF = 1e18, N = 2e5+5;
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
 
 typedef vector<int> vi;
 typedef pair<int, int> pii;
 typedef vector<pii> vpi;
 /*
- ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════ 
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
 */
 signed main()
 {
@@ -41,7 +44,7 @@ signed main()
     }
     else
     {
-        cout << a /  b << endl;
+        cout << a / b << endl;
     }
 
     return 0;
