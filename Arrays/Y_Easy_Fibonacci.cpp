8076c2aa0dc9cc0c9ce74f1c8904f7ea5e4e7e09
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-27 21:42:28 +0600

    Initial commit

---
+++ Arrays/Y_Easy_Fibonacci.cpp
@@ -0,0 +1,42 @@
+/*
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 27-07-2025  21:29:19
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
+signed main()
+{
+    fastIO;
+    int n, a = 0, b = 1;
+    cin >> n;
+    while (n--)
+    {
+        cout << a << " ";
+        int s = a + b;
+        a = b;
+        b = s;
+    }
+    cout << endl;
+
+    return 0;
+}
